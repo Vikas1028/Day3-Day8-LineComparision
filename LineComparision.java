9481b28798703ee2397f9505d95d6f663c895b84
Vikas1028 <vikasbhandekar2018@gmail.com>
2022-09-30 00:23:45 +0530

    Add: Welcome Message

---
+++ LineComparision.java
@@ -0,0 +1,14 @@
+package LineComparision;
+
+public class LineComparision {
+
+	public static void main(String[] args) {
+		LineComparision call = new LineComparision();
+		call.printWelcomeMessage();
+	}
+
+	// welcome message
+	public void printWelcomeMessage() {
+		System.out.println("Welcome to Line Comparison Computation Program");
+	}
+}
